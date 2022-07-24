ff586b46ddd89f2af2e8d815f5e88e0b216396d3
Lucas <lucasmsaldana@gmail.com>
2022-07-24 14:22:56 -0300

    CodeWars, ejercicios diarios.

---
+++ src/ReversedStrings.java
@@ -0,0 +1,13 @@
+public class ReversedStrings {
+    //Complete the solution so that it reverses the string passed into it.
+    public static void main(String[] args) {
+
+        String texto = "Hola Mundo";
+        String reversedTexto="";
+
+        for(int i=texto.length()-1; i>=0;i--){
+            reversedTexto+=texto.charAt(i);
+        }
+
+    }
+}
