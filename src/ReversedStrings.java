c4e557ec9926da0c667874a8b854546a8b3259b7
Lucas <lucasmsaldana@gmail.com>
2022-07-24 14:29:37 -0300

    CodeWars, ejercicios diarios.

--- src/ReversedStrings.java
+++ src/ReversedStrings.java
@@ -4,11 +4,24 @@ public class ReversedStrings {
 
         String texto = "Hola Mundo";
         String reversedTexto="";
+        System.out.println("**********************************");
+        System.out.println("********Resolucion con For********");
+        System.out.println("**********************************");
+        System.out.println("");
 
         for(int i=texto.length()-1; i>=0;i--){
             reversedTexto+=texto.charAt(i);
         }
         System.out.println("Texto original : "+texto);
         System.out.println("Texto dado vuelta : "+reversedTexto);
+        System.out.println("");
+        // Resolucion con StringBuilder
+
+        System.out.println("**********************************");
+        System.out.println("***Resolucion con StringBuilder***");
+        System.out.println("**********************************");
+        System.out.println("");
+        System.out.println("Texto original : "+texto);
+        System.out.println("Texto dado vuelta : "+new StringBuilder(texto).reverse());
     }
 }
