b2da49e773fdecb35f971a8b66c4e12b898f3c7f
Lucas <lucasmsaldana@gmail.com>
2022-08-02 15:18:33 -0300

    CodeWars, ejercicios diarios.

--- src/SquareEveryDigit.java
+++ src/SquareEveryDigit.java
@@ -1,2 +1,13 @@
 public class SquareEveryDigit {
+    public static void main(String[] args) {
+        Integer n = 9119;
+        String nText=String.valueOf(n);
+        String textoAconvertir="";
+        for(int i=0; i<nText.length();i++){
+            int numero = (int) Math.pow(Integer.parseInt(nText.substring(i,i+1)),2);
+            textoAconvertir+=String.valueOf(numero);
+        }
+        System.out.println(Integer.parseInt(textoAconvertir));
+
+    }
 }
