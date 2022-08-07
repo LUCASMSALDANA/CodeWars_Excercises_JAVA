9c9e3ebfb7ad0f61b4a2af27af0a98b84597a145
Lucas <lucasmsaldana@gmail.com>
2022-08-07 13:12:17 -0300

    CodeWars, ejercicios diarios.

--- src/SnailSort.java
+++ src/SnailSort.java
@@ -1,6 +1,13 @@
+import java.util.Arrays;
+
 public class SnailSort {
     public static void main(String[] args) {
-        int[][]array = new int[4][4];
+        int[][]array = {{1,2,3},{8,9,4},{7,6,5}};
+        int[] newArray = new int[array.length* array[0].length];
+        for(int i =0; i< array[1].length;i++){
+
+        }
+        System.out.println(newArray.length);
 
     }
 
