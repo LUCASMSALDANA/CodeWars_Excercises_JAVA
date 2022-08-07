67265f6fcf042c86687834cfc22098a2a3fab1f5
Lucas <lucasmsaldana@gmail.com>
2022-08-07 12:50:18 -0300

    CodeWars, ejercicios diarios.

---
+++ src/SnailSort.java
@@ -0,0 +1,7 @@
+public class SnailSort {
+    public static void main(String[] args) {
+        int[][]array = new int[4][4];
+
+    }
+
+}
--- src/SquareEveryDigit.java
+++ src/SquareEveryDigit.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class SquareEveryDigit {
     public static void main(String[] args) {
         Integer n = 9119;
@@ -7,7 +10,18 @@ public class SquareEveryDigit {
             int numero = (int) Math.pow(Integer.parseInt(nText.substring(i,i+1)),2);
             textoAconvertir+=String.valueOf(numero);
         }
-        System.out.println(Integer.parseInt(textoAconvertir));
+       // System.out.println(Integer.parseInt(textoAconvertir));
+        int numeros[] = {1,2,3,4,5,6,7,8,9,10,11,12};
+        int numero=prueba(numeros);
 
+
+    }
+
+    private static int prueba(int[] numeros) {
+        for (int i=0; i<numeros.length;i++){
+            System.out.println(numeros[i]);
+            return numeros[i];
+        }
+        return 0;
     }
 }
